f8724edbd9bad81765ee5f15354017c136067c16
Zhanpeng Danny Zhang <65028732+dannyzzp@users.noreply.github.com>
2021-08-07 17:05:47 -0700

    Update showGraphs.R

--- showGraphs.R
+++ showGraphs.R
@@ -1,27 +1,35 @@
-df=read.csv('C:/Users/danny/OneDrive/??????/Python Scripts/covid_across_us/covid.csv',row.names = 1)
+df_raw=read.csv('C:/Users/danny/OneDrive/文档/Python Scripts/covid_across_us/covid.csv',row.names = 1)
 library(dplyr)
-df=df%>%
-  arrange(desc(cases))
-df$state=factor(df$state,levels=df$state)
-df$group_by_cases[1:nrow(df)]=0
-groupSize=ceiling(nrow(df)/3)
-df[1:groupSize,'group_by_cases']='High'
-df[(groupSize+1):(2*groupSize),'group_by_cases']="Middle"
-df[(groupSize*2+1):nrow(df),'group_by_cases']="Low"
-df$group_by_cases=factor(df$group_by_cases,levels=c('High','Middle','Low'))
+library(tidyr)
 library(ggplot2)
-library(ggthemes)
-total_cases=sum(df['cases'])
+order=df_raw%>%
+  arrange(desc(cases))%>%
+  mutate(state=factor(state,levels=order$state))
+
+df_raw=df_raw[match(order$state,df_raw$state),]
+
+df<-df_raw%>%
+  select(-vaccinationsInitiated)%>%
+  gather(key='data',value='value',cases:deaths)%>%
+  mutate(state=factor(state,levels=order$state))
+total_cases=sum(df_raw['cases'])
 title=paste("COVID Data Around USA    Total Confirmed Cases: ",total_cases)
 ggplot(df)+
-  geom_col(aes(x=state,y=cases,fill=group_by_cases))+
-  geom_col(aes(x=state,y=deaths),fill='violetred4')+
-  scale_fill_manual('Confirmed Cases',values=c('blue4','blue3','blue2'))+
-  geom_line(aes(x=state,y=vaccinationsCompleted),group=1,color='springgreen1',show.legend = TRUE)+
+  geom_col(aes(x=state,y=value,fill=data))+
+  scale_fill_manual(values=c("#00BFC4", "#F8766D"))+
+  geom_line(aes(x=state,y=vaccinationsCompleted),color='#7CAE00',group=1,show.legend = TRUE)+
+  scale_color_manual("Fully Vaccinated", values=c(Fully_Vaccinated="#7CAE00"))+
   ggtitle(title)+
   theme(plot.title = element_text(hjust = 0.5))+
   xlab('States')+
-  ylab('Counts')+
-  ylim(c(0,20961749))+
-  scale_color_manual("Fully Vaccinated", values=c(green="green"))
+  theme_bw()+
+  scale_y_continuous(
+    
+    # Features of the first axis
+    name = "Counts",
+    
+    # Add a second axis and specify its features
+    sec.axis = sec_axis( trans=~./331449281*100, name="Percentage %")
+  )
 
+    
